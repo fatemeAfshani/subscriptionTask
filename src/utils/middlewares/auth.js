7b05140b69a0c88310c52b8584bccfc361c0ea7d
f.afshani <fatemeh.afshani78@gmail.com>
2023-03-03 17:35:51 +0330

    feat: add authentication middleware

---
+++ src/utils/middlewares/auth.js
@@ -0,0 +1,23 @@
+const jwt = require("jsonwebtoken");
+const logger = require("../logger");
+const customerDB = require("../database/customer");
+
+module.exports.isAuth = async (req, res, next) => {
+  try {
+    const authorization = req.header("Authorization")?.replace("Bearer ", "");
+    const { data } = jwt.verify(authorization, process.env.JWT_TOKEN);
+    if (!data) return res.sendStatus(401);
+
+    const user = (
+      await customerDB.get({ username: data.username, id: data.id })
+    )?.[0];
+    if (!user) return res.sendStatus(401);
+
+    req.token = authorization;
+    req.user = user;
+    next();S
+  } catch (error) {
+    logger.error(`error in authorization middleware, ${error}`);
+    return res.sendStatus(401);
+  }
+};
